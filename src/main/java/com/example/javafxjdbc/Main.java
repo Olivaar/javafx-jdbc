9f03f76c8b06355ace2a7ec48619cd5017192ef2
Olivar Baloi <mocolivarbaloi@gmail.com>
2022-01-24 21:51:42 -0300

    Project Configurantion
    ADD Libraries javafx and Mysql Connector

---
+++ src/main/java/com/example/javafxjdbc/Main.java
@@ -0,0 +1,31 @@
+package com.example.javafxjdbc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+
+}
